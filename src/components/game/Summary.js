8ebb96d30cb9742d6da93769bd0b9e6b2a826d3c
Fernando Lopez <fernando.lopez@learnifier.com>
2020-11-01 16:59:32 +0100

    added page summary and router for it

---
+++ src/components/game/Summary.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import {Container, Row, Col, Button} from 'reactstrap';
+
+class Summary extends Component {
+  render () {
+    return(
+    <>
+      <Container>
+        <Row>
+          <Col>
+            summary
+          </Col>
+        </Row>
+      </Container>
+      </>
+    )
+  }
+}
+export default Summary;
