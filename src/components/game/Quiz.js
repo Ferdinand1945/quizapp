145e3abfb6f26556fb0d9dfcebdc50b6eb08f7c2
Fernando Lopez <fernando.lopez@learnifier.com>
2020-11-01 18:31:41 +0100

    added some  design and the first states

--- src/components/game/Quiz.js
+++ src/components/game/Quiz.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import {Container, Row, Col} from 'reactstrap'
-class Quiz extends React.Component {
+import React, {Component} from 'react';
+import {Container, Row, Col, Button} from 'reactstrap'
+import quiz from '../../quiz.json';
+
+class Quiz extends Component {
+  constructor (props){
+    super(props);
+      this.state = {
+        quiz,
+        answer: '',
+        currentQ: {},
+        nextQ: {},
+        previousQ: {},
+        numberofQ: 0,
+        numberofAnsweredQ: 0,
+        score: 0,
+    }
+  }
   render() {
+    console.log(quiz)
     return(
     <>
       <Container>
         <Row>
-          <Col>Play the quiz</Col>
+            <Col md={'10'}>
+              <div className="panel panel-primary">
+                <h3 className="panel-title">
+                  Quiz
+                </h3>
+                <div className="panel-body">
+                  <p>Time remain 0:15  </p>
+                  <p>50/50</p>
+                  <p>+10 sec</p>
+                  <div>
+                    <p>The question</p>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                  </div>
+                <div>
+                  <Button id="previous">
+                    Previous
+                  </Button>
+                  <Button id="next">
+                    Next
+                  </Button>
+                  <Button className={'btn btn-danger ml-2'} id="quit">
+                    Quit
+                  </Button>
+                </div>
+                </div>
+              </div>
+            </Col>
         </Row>
       </Container>
     </>
